3085a9f0dbee281a984fe9bf492449c84d751d85
JinH <jinh@tistory.com>
2013-03-27 19:53:39 +0900

    업데이트

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+function forIE() { //creat html5 element for IE
+	if(!/*@cc_on!@*/0) return;
+	var e = "abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,footer,hgroup,header,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=0,length=e.length;
+	while(i<length){
+		document.createElement(e[i++]);
+	}
+}
+
+function prettyprint_class_add() {
+	$('code').addClass('prettyprint');
+	$('code > xmp').addClass(function(){$(this).parent().removeClass('prettyprint'); return 'prettyprint';});
+	$('code > pre').addClass(function(){$(this).parent().removeClass('prettyprint'); return 'prettyprint';});
+	$('pre > code').removeClass(function(){$(this).parent().addClass('prettyprint'); return 'prettyprint';});
+}
+
+$(function() {
+	prettyprint_class_add(); prettyPrint(); //code prettify (.prettyprint)
+	$('.ad_article_mobile').remove();
+});

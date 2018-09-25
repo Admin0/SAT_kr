6c1b61a19ca503ff7e38606ec549cfdbb5aacd13
JinH <jinh719@gmail.com>
2018-09-25 20:08:25 +0900

    wow nice!

--- js/main.js
+++ js/main.js
@@ -8,26 +8,9 @@ function forIE() { //creat html5 element for IE
   }
 }
 
-function prettyprint_class_add() {
-  $('code').addClass('prettyprint');
-  $('code > xmp').addClass(function() {
-    $(this).parent().removeClass('prettyprint');
-    return 'prettyprint';
-  });
-  $('code > pre').addClass(function() {
-    $(this).parent().removeClass('prettyprint');
-    return 'prettyprint';
-  });
-  $('pre > code').removeClass(function() {
-    $(this).parent().addClass('prettyprint');
-    return 'prettyprint';
-  });
-}
-
 $(function() {
-  prettyprint_class_add();
-  prettyPrint(); //code prettify (.prettyprint)
-  $('.ad_article_mobile').remove();
+  new ClipboardJS('.copythis');
+  // $('.ad_article_mobile').remove();
 });
 
 // $.ajax({
